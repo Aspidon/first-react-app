4b8057440490e967572aaad5e677a255daf735fa
Aspidon <aleksandr-stec@mail.ru>
2020-09-18 16:08:19 +0300

    First App of React - lesson 1

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app-header.css';
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Aspidon</h1>
+            <h2>ReactBook, 5 записей, понравилось 3</h2>
+        </div>
+        )
+}
+
+export default AppHeader;
