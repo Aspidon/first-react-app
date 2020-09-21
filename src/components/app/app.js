db64f9b562bc964731dc9ac36158d37960e57e76
Alex <aleksandr-stec@mail.ru>
2020-09-21 20:21:26 +0300

    2020-09-21 93 lesson

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,17 +8,43 @@ import PostAddForm from '../post-add-form';
 
 import './app.css';
 
+import styled from 'styled-components';
+
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`
+
 const App = () => {
+
+    let data =[
+        {
+            label: "React RulezzzZ",
+            important: true,
+            id: 3
+        },
+        {
+            label: "CSS Cool",
+            important: false,
+            id: 2
+        },
+        {
+            label: "JS is Good",
+            important: false,
+            id: 1
+        }
+    ];
+
     return (
-        <div className='app'>
+        <AppBlock>
             <AppHeader/>
             <div className='search-panel d-flex'>
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
-        </div>
+        </AppBlock>
     )
 }
 
--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './post-list-item.css';
+export default class PostListItem extends Component {
 
-const PostListItem = () => {
-    return (
-        <li className='app-list-item d-flex justify-content-between'>
-            <span className='app-list-item-label'>
-                Hello World
+    constructor(props) {
+        super(props);
+        this.state = {
+            important:false,
+            like: false
+        };
+        this.onImportant = this.onImportant.bind(this);
+        this.onLike = this.onLike.bind(this);
+    }
+
+    onImportant() {
+        this.setState(({important}) => ({
+            important: !important
+        }))
+    }
+
+    onLike() {
+        this.setState(({like}) => ({
+            like: !like
+        }))
+    }
+
+    render() {
+
+        const {label} = this.props;
+        const {important, like} = this.state;
+
+        let classNames = 'app-list-item d-flex justify-content-between';
+        classNames = important ? classNames + " important" : classNames;
+        classNames = like ? classNames + " like" : classNames;
+
+        return (
+            <li className={classNames}>
+            <span className='app-list-item-label' onClick={this.onLike}>
+                {label}
             </span>
             <div className='d-flex justify-content-center align-item-center'>
-                <button type='button' className='btn-star btn-sm'>
+                <button type='button' className='btn-star btn-sm' onClick={this.onImportant}>
                     <i className='fa fa-star'></i>
                 </button>
                 <button type='button' className='btn-trash btn-sm'>
                     <i className='fa fa-trash-o'></i>
                 </button>
-                <i className='fa fa-heart-o'></i>
+                <i className='fa fa-heart'></i>
             </div>
         </li>
-    )
+        )
+    }
 }
-
-export default PostListItem;
